5cc506eec0ef99fdd32f80fa4b09bfc8bcdc4255
kyun9 <gusl108@naver.com>
2019-07-15 21:15:38 +0900

    context.getRealPath("/") 위치 확인하기
    확인하기

--- src/main/java/my/spring/mini/GroupController.java
+++ src/main/java/my/spring/mini/GroupController.java
@@ -2,6 +2,7 @@ package my.spring.mini;
 
 import java.io.IOException;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
